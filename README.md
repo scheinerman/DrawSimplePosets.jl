6be987c18babe1625e3cef380c7be6d78c36dcf2
Ed Scheinerman <ers@jhu.edu>
2024-02-16 14:52:43 -0500

    Baby steps

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DrawSimplePosets
 Create and draw Hasse diagrams. This is a companion to `DrawSimpleGraphs`.
+
+
+## Basic Functions
+
+This is completely aspirational.
+
+* `H = HasseDiagram(P::SimplePoset)` initiate a Hasse digram. 
+* `draw(H)` draws `H` on the screen.
--- src/DrawSimplePosets.jl
+++ src/DrawSimplePosets.jl
@@ -1,5 +1,24 @@
 module DrawSimplePosets
 
-greet() = print("Hello World!")
+using SimplePosets, SimpleGraphs, DrawSimpleGraphs, SimpleDrawing
+
+import SimpleDrawing: draw
+export draw
+
+export HasseDiagram
+
+struct HasseDiagram{T}
+
+    P::SimplePoset{T}
+    G::UndirectedGraph{T}
+
+    function HasseDiagram(PP::SimplePoset{T}) where T
+        G = simplify(CoverDigraph(PP))
+        embed(G)
+        new{T}(PP,G)
+    end
+end
+
+draw(H::HasseDiagram) = draw(H.G)
 
 end # module DrawSimplePosets
