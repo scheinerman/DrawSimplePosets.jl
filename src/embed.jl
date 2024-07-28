55308a585db00fcf480ac414c3de63f49b107a7d
Ed Scheinerman <ers@jhu.edu>
2024-07-28 14:59:18 -0400

    Split out embedding methods

--- src/embed.jl
+++ src/embed.jl
@@ -1,5 +1,3 @@
-export basic_embedding
-
 export setxy!, setx!, sety!
 
 """
@@ -56,26 +54,4 @@ end
 
 
 
-
-"""
-    basic_embedding(P::SimplePoset{T})::Dict{T, Vector{Float64}} where T
-
-Create a basic embedding for `P`.
-"""
-function basic_embedding(P::SimplePoset{T})::Dict{T,Vector{Float64}} where {T}
-    PP = deepcopy(P)
-    d = Dict{T,Vector{Float64}}()
-    y = 0
-    while card(PP) > 0
-        y += 1
-        bot = minimals(PP)
-        nbot = length(bot)  # number of minimals
-        for j = 1:nbot
-            x = j - nbot / 2 - 0.5
-            v = bot[j]
-            d[v] = [x, y]
-            delete!(PP, v)
-        end
-    end
-    return d
-end
+include("basic_embedding.jl")
